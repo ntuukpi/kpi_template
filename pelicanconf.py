32c3b929203f9afae3f52e7c9b9c4372091fca11
Gregory Reshetniak <gregory.reshetniak@automatic.com>
2016-06-16 13:28:24 +0200

    first template upload

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'ror6ax'
+SITENAME = u'kpi'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+THEME = 'Flex'
+DEFAULT_LANG = u'ua'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('kpi.ua', 'http://kpi.ua'),)
+
+# Social widget
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
